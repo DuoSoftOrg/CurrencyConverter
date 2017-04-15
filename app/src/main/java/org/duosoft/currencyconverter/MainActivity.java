a7444a5b8e16a5d5746c5bec1db49a3dbb431f2d
DuoSoftOrg <rnteam@yandex.ru>
2017-04-15 19:23:59 +0300

    Чистый проект

---
+++ app/src/main/java/org/duosoft/currencyconverter/MainActivity.java
@@ -0,0 +1,21 @@
+package org.duosoft.currencyconverter;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorAccent));
+        }
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+    }
+}
