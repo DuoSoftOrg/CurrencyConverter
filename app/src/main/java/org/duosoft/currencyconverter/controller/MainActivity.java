f553833363fadd8eb57ac3aafb915d57d024155d
Arafor <rnteam@yandex.ru>
2017-04-17 10:16:14 +0300

    Revert ":circus_tent: Added .gitattributes"
    This reverts commit f673c63211b17d06ae45547a0725cb2c0079095d.

--- app/src/main/java/org/duosoft/currencyconverter/controller/MainActivity.java
+++ app/src/main/java/org/duosoft/currencyconverter/controller/MainActivity.java
@@ -1,10 +1,12 @@
-package org.duosoft.currencyconverter;
+package org.duosoft.currencyconverter.controller;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.duosoft.currencyconverter.R;
 
 public class MainActivity extends Activity {
 
